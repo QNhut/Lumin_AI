79d6d6e5e36ad53ab0b90a255c6e43696c8a2329
phongnh2 <phongnh@luminpdf.com>
2026-01-07 13:51:10 +0700

    ✨ feat: add ESLint and Prettier configuration files

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,86 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
+      import: importPlugin,
+      prettier: prettierPlugin,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'arrow-body-style': ['error', 'as-needed'],
+      'prettier/prettier': [
+        'error',
+        {
+          tabWidth: 2,
+          printWidth: 120,
+          useTabs: false,
+          semi: true,
+          bracketSameLine: false,
+          singleQuote: true,
+        },
+      ],
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'import/no-cycle': ['error', { maxDepth: 10 }],
+      'import/order': [
+        'warn',
+        {
+          groups: [['external', 'builtin'], 'internal', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            { pattern: '@/assets/**', group: 'internal', position: 'after' },
+            { pattern: '@/components/**', group: 'internal', position: 'after' },
+            { pattern: '@/pages/**', group: 'internal', position: 'after' },
+            { pattern: '@/styles/**', group: 'internal', position: 'after' },
+            { pattern: '@/**', group: 'internal', position: 'after' },
+            { pattern: './*.module.scss', group: 'sibling', position: 'after' },
+            { pattern: './*.scss', group: 'sibling', position: 'after' },
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      // Enforce Prettier formatting rules as ESLint errors
+      indent: ['error', 2],
+      'max-len': ['error', { code: 120 }],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'jsx-quotes': ['error', 'prefer-double'],
     },
   },
-])
+];
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,49 +1,46 @@
-import { NavLink, useNavigate } from "react-router-dom"
-import { SlArrowDown } from "react-icons/sl";
-import styles from "./Header.module.scss"
-import logo from "./logo.png"
+import { useNavigate } from 'react-router-dom';
+
+import logo from './logo.png';
+
+import styles from './Header.module.scss';
 
 export default function Header() {
-    const navigator = useNavigate();
-    return (
-       <header className={styles.navbar}>
-        <div className={styles.navbar__left}>
-            {/* Logo */}
-            <div className={styles.navbar__logo} onClick={() => navigator("/")}>
-                <img src={logo} alt="Logo" />
-            </div>
-            {/* Menu */}
-            <nav className={styles.navbar__menu}>
-                <NavLink to= "/products" className={styles.navbar__link}>
-                    Products
-                    <SlArrowDown className="navbar__icon"/>
-                </NavLink>
-                <NavLink to= "/capabilities" className={styles.navbar__link}>
-                    Capabilities
-                    <SlArrowDown className="navbar__icon"/>
-                </NavLink>
-                <NavLink to= "/solutions" className={styles.navbar__link}>
-                    Solutions
-                    <SlArrowDown className="navbar__icon"/>
-                </NavLink>
-                <NavLink to= "/learn_Discover" className={styles.navbar__link}>
-                    Learn & Discover
-                    <SlArrowDown className="navbar__icon"/>
-                </NavLink>
-                <NavLink to= "/pricing" className={styles.navbar__link}>
-                    Pricing
-                </NavLink>
-            </nav>
+  const navigator = useNavigate();
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.navbar__left}>
+        <div className={styles.navbar__logo} onClick={() => navigator('/')}>
+          <img src={logo} alt="Logo" />
         </div>
-
-        {/* Button */}
-        <div className={styles.navbar__actions}>
-            <NavLink to= "/request" className = {styles.navbar__link}>
-                Request a demo
-            </NavLink>
-            <h3>|</h3>
-            <button className={styles.btn}>Try for free</button>
-        </div>
-       </header>
-    )
+        <nav className={styles.navbar__menu}>
+          <NavLink to="/products" className={styles.navbar__link}>
+            Products
+            <SlArrowDown className="navbar__icon" />
+          </NavLink>
+          <NavLink to="/capabilities" className={styles.navbar__link}>
+            Capabilities
+            <SlArrowDown className="navbar__icon" />
+          </NavLink>
+          <NavLink to="/solutions" className={styles.navbar__link}>
+            Solutions
+            <SlArrowDown className="navbar__icon" />
+          </NavLink>
+          <NavLink to="/learn_Discover" className={styles.navbar__link}>
+            Learn & Discover
+            <SlArrowDown className="navbar__icon" />
+          </NavLink>
+          <NavLink to="/pricing" className={styles.navbar__link}>
+            Pricing
+          </NavLink>
+        </nav>
+      </div>
+      <div className={styles.navbar__actions}>
+        <NavLink to="/request" className={styles.navbar__link}>
+          Request a demo
+        </NavLink>
+        <h3>|</h3>
+        <button className={styles.btn}>Try for free</button>
+      </div>
+    </header>
+  );
 }
